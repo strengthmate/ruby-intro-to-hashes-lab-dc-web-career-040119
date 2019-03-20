6f2ae08d74f4125a0aa7647dfb011121b26cef78
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 21:24:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,7 @@ def monopoly_with_second_tier
          :shortline => {}
 	     },
 	     
-       :rent_in_dollars => {      
-         :four_pieces_owned => 200,
-         :one_piece_owned => 25,
-         :three_pieces_owned => 100,
-         :two_pieces_owned => 50
-       }
+       :rent_in_dollars => {}
        
     }
   }
@@ -36,6 +31,11 @@ end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	
+	         :four_pieces_owned => 200,
+         :one_piece_owned => 25,
+         :three_pieces_owned => 100,
+         :two_pieces_owned => 50
 
 end
 
