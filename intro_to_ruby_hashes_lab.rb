d07ca5bd281480a040a4440ed5f568ab8ef86640
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 21:12:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,11 +13,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
-	     :pieces => 4,
-       :rent_in_dollars => {
-       }
-       :names => {
-       } 
+	     :pieces => '4',
+       :rent_in_dollars => {}
+       :names => {}
     }
   }
 end
